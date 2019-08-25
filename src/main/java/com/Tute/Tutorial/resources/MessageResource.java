a65be8cae17e3c323d279716be77561f5f0a0781
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-08-25 09:41:09 +0530

    create resource

---
+++ src/main/java/com/Tute/Tutorial/resources/MessageResource.java
@@ -0,0 +1,16 @@
+package com.Tute.Tutorial.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/message")
+public class MessageResource {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMessage() {
+		return "get message works";
+	}
+}
